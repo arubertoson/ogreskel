a4e3978d6bf34a438efbbd04e01ce99312ad05ed
Albertsson, Marcus <marcus.albertsson@otto.de>
2020-01-29 19:25:34 +0100

    .

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+
+#include <OgreSkeleton.h>
+
+
+#include <iostream>
+
+int main() {
+  std::cout << "Hello" << std::endl;
+
+  std:cout << Ogre::SkeletonAnimationBlendMode::ANIMBLEND_AVERAGE << std::endl;
+
+}
